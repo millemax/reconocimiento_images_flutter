d6eb20e8fcfe516cab09549992df1b4344bcb668
millermax <romel_23_3@hotmail.com>
2020-08-19 17:34:56 -0500

    splash esta funcionando

---
+++ lib/pages/splash.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+
+class Splashscreen extends StatefulWidget {
+  Splashscreen({Key key}) : super(key: key);
+
+  @override
+  _SplashscreenState createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+   // Timer(Duration(seconds:11), ()=> Navigator.push(context,MaterialPageRoute(builder:(context)=> LoginPage())));
+        
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.green[500],
+          padding: EdgeInsets.only(top: 250),
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            children: [
+              Image.asset('assets/images/logoblanco.png', height: 150), 
+              SizedBox(height:20),
+              Text('Medic Plant', style: TextStyle(color: Colors.white)),
+              
+            ],
+          ),
+        ),
+      
+
+
+      
+
+       
+    );
+  }
+}
+
+
